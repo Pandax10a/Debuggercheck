f59b89b4ec4415bed7c2e8d28beea3a85b732273
panda1 <blueblue0011@gmail.com>
2022-07-07 18:23:57 -0700

    going to test debugging

---
+++ app.js
@@ -0,0 +1,11 @@
+let username = "alphaomega";
+let user_age = 22;
+let is_member = true;
+
+if (username === "admin") {
+    console.log("Greetings Admin!!");
+} else if ((user_age >= 30) && (is_member === true)) {
+    console.log("Greetings master");
+} else {
+    console.log("I'm going to put myself in suspend mode now");
+}
